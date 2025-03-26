ec82e3f586aadc04e2a48424dffbc18a380609e6
johnMontyc <johnfe2003@gmail.com>
2025-03-25 21:39:04 -0500

    Creacion del backend del proyecto

---
+++ src/main/java/com/zeal/zeal/ZealApplication.java
@@ -0,0 +1,13 @@
+package com.zeal.zeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeal/zeal/ZealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeal.zeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
